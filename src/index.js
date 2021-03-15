0c7afa0cf298a8f9692a8114c863328a0eeabc21
AleksandrChuprina <ichupria@gmail.com>
2021-03-15 22:28:13 +0300

    init: complete brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 == 0) {
+    for (let i = str.length - 1; i >= 0; i--) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if (str[i] == bracketsConfig[j][0] || str[i] == bracketsConfig[j][1]) {
+          let a = bracketsConfig[j].join('');
+          str = str.replace(a,'');
+        }
+      }
+    }
+    if (str.length == 0) {
+      return true;
+    }
+  }
+  return false;
 }
